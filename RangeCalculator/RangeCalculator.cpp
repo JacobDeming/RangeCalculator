6747ed32aca11ec5a4a4fe10788bf6a1f154c695
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-11-03 15:33:45 -0400

    First commit

--- RangeCalculator/RangeCalculator.cpp
+++ RangeCalculator/RangeCalculator.cpp
@@ -1,11 +1,24 @@
 // RangeCalculator.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
 #include "stdafx.h"
 
 
 int main()
 {
+	int start = 0, finish = 0, sum = 0;
+	std::cout << "Please enter two numbers: " << std::endl;
+	std::cin >> start >> finish;
+	int count = start;
+	while (count <= finish)
+	{
+		sum += count;
+		++count;
+	}
+
+	std::cout << "The sum of the range between " << start << " and " << finish << " is " << sum << std::endl;
+
     return 0;
 }
 
