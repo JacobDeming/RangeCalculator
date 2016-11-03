6f2fba7331bdd5fa59b4c93eebb3a8f337f75bdc
Jacob "TKC" Deming <JacobDeming91@gmail.com>
2016-11-03 17:38:31 -0400

    Checks for proper input values

--- RangeCalculator/RangeCalculator.cpp
+++ RangeCalculator/RangeCalculator.cpp
@@ -10,14 +10,17 @@ int main()
 	int start = 0, finish = 0, sum = 0;
 	std::cout << "Please enter two numbers: " << std::endl;
 	std::cin >> start >> finish;
-	int count = start;
-	while (count <= finish)
-	{
-		sum += count;
-		++count;
+	if (start >= finish) {
+		std::cout << "Improper startpoint and endpoint. Please try again.";
+	} else {
+		int count = start;
+		while (count <= finish)
+		{
+			sum += count;
+			++count;
+		}
+		std::cout << "The sum of the range between " << start << " and " << finish << " is " << sum << std::endl;
 	}
-
-	std::cout << "The sum of the range between " << start << " and " << finish << " is " << sum << std::endl;
 
     return 0;
 }
